b9762450bd711eb40884bb31cb9d61c8d74c7026
Rohan <Rohan>
2016-01-05 18:25:03 +0530

    who doesnt like clean code?

--- client.js
+++ client.js
@@ -5,27 +5,22 @@ var PORT = 6969;
 
 var client = new net.Socket();
 var client = new net.Socket();
-client.connect(PORT, HOST, function() 
-                           {
-                              console.log('CONNECTED TO: ' + HOST + ':' + PORT); 
+client.connect(PORT, HOST, function() {
+    console.log('CONNECTED TO: ' + HOST + ':' + PORT);
 
-                              process.stdin.on('readable', function() 
-			     			 	                                         {
-  		             		 			                                   var chunk = process.stdin.read();
-  			        				                                       if (chunk !== null)
-								                                               {
-	    								                                            client.write(chunk);
-  								                                             }
-			      			 	                                       });
-			                        
-                              client.on('data', function(data)
-                  	      			                {
-    							                                  console.log('Server:' + data);
-		                  	                        });  
-			                        
-                              client.on('close', function()
-                                                 {
-                                                    console.log('Connection closed');
-                                                 });
+    process.stdin.on('readable', function() {
+        var chunk = process.stdin.read();
+        if (chunk !== null) {
+            client.write(chunk);
+        }
+    });
 
-                           });
+    client.on('data', function(data) {
+        console.log('Server:' + data);
+    });
+
+    client.on('close', function() {
+        console.log('Connection closed');
+    });
+
+});
--- server.js
+++ server.js
@@ -2,37 +2,30 @@ var net = require('net');
 var fs = require('fs');
 
 var HOST = '0.0.0.0';
- PORT = 6969;
+PORT = 6969;
 
-net.createServer(function(sock)
-                 {
-                      sock.on('data', function(data)
-                                      {
-                                          //console.log('Client:'+ data);
-                                          fs.appendFile("/home/varunited/Desktop/Socket/test/log.txt", data , function(err)
-                                                                                  {
-                                                                                      if(err)
-                                                                                      {
-                                                                                          return console.log(err);
-                                                                                      }
+net.createServer(function(sock) {
+    sock.on('data', function(data) {
+        //console.log('Client:'+ data);
+        fs.appendFile("/home/varunited/Desktop/Socket/test/log.txt", data, function(err) {
+            if (err) {
+                return console.log(err);
+            }
 
-                                       //                                               console.log("The file was saved!");
-                                                                                  }); 
-                                      });
-                    
-                      process.stdin.on('readable', function() 
-			     			                                   {
-  		             			 	                            var chunk = process.stdin.read();
-  			     				                                  if (chunk !== null)
-							                                        {
-	    							                                      sock.write(chunk);
-  							                                      }
-			     			                                   });
-    
-                      sock.on('close', function(data)
-                                       {
-                                          console.log('Client Disconnected');
-                                       });
-                 }).listen(PORT, HOST);
+            //console.log("The file was saved!");
+        });
+    });
 
-console.log('Server listening on ' + HOST +':'+ PORT);
+    process.stdin.on('readable', function() {
+        var chunk = process.stdin.read();
+        if (chunk !== null) {
+            sock.write(chunk);
+        }
+    });
+
+    sock.on('close', function(data) {
+        console.log('Client Disconnected');
+    });
+}).listen(PORT, HOST);
+
+console.log('Server listening on ' + HOST + ':' + PORT);
